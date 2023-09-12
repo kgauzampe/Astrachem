27f2ac77c43c0f78791d7bc9183974939c26a60c
Mpumelelo Kgaugelo Mpe <53508326+kgauzampe@users.noreply.github.com>
2023-09-12 14:37:17 +0200

    Files created

---
+++ src/components/Home/home.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Navbar from "../navigation/navigation";
+import Footer from "../footer/footer";
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import One from "../../images/one.jpg"
+import Two from "../../images/two.jpg"
+import Three from "../../images/three.jpg"
+import Four from "../../images/four.jpg"
+import './home.css'
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+function Home() {
+
+    return (
+        <div className="Home">
+            <Navbar />
+            <div className="Home-part">
+                <div className="home-text">
+                    <Typography variant="h2" component="h2" sx={{ fontWeight: 'bold' }}>
+                        Africa's Reliable Hygeine Partner
+                    </Typography>
+                    <Typography variant="h4" component="h2">
+                        Elevate Your Standards With Our Premium Cleaning Solutions
+                    </Typography>
+                </div>
+            </div>
+            <div className="sector">
+                <Typography variant="h4" component="h2" sx={{ fontWeight: 'bold' }} className="sectorText">
+                    Choose Your Sector and Explore Expert Selected Products
+                </Typography>
+                <div className="sectorDiv">
+                    <Box sx={{ width: '30%' }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={6}>
+                                <img src={Three} alt="one" className="one" />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <img src={Two} alt="one" className="one" />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <img src={Three} alt="one" className="one" />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <img src={Two} alt="one" className="one" />
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Link as ScrollLink } from 'react-scroll';
+// import logo from '../../../Images/logox.png'
+import './navigation.css';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="logo">
+      {/* <img src={logo} alt="logo" className="logo" /> */}
+      </div>
+      <div className={`menu ${isOpen ? 'active' : ''}`}>
+        <ul>
+          <li>
+            <Link to="/" spy={true} smooth={true} duration={500} className='link'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <ScrollLink to="abtBanner" spy={true} smooth={true} duration={500}>
+              About
+            </ScrollLink>
+          </li>
+          <li className="dropdown">
+            <span>Services</span>
+            <div className="dropdown-content">
+              <Link to="/service1" spy={true} smooth={true} duration={500}>
+                Software QA Training
+              </Link>
+              <Link to="/service2" spy={true} smooth={true} duration={500}>
+                IT Training
+              </Link>
+              <Link to="/service3" spy={true} smooth={true} duration={500}>
+                IT Consultancy
+              </Link>
+              <Link to="/service4" spy={true} smooth={true} duration={500}>
+                Software QA Consultancy
+              </Link>
+            </div>
+          </li>
+          <li>
+            <Link to="/Contact" spy={true} smooth={true} duration={500} className='link'>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="burger-menu" onClick={toggleMenu}>
+        <div className={`bar ${isOpen ? 'active' : ''}`} />
+        <div className={`bar ${isOpen ? 'active' : ''}`} />
+        <div className={`bar ${isOpen ? 'active' : ''}`} />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
